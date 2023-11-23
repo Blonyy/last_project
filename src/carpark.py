5c8ffd5e08b3440d15241192d204637029db6c3b
Ninii2 <blonexmaunela@gmail.com>
2023-11-23 15:06:36 +0800

    Added downloaded guide

--- src/carpark.py
+++ src/carpark.py
@@ -1,10 +1,10 @@
 class Carpark:
-    def __init__(self, location="Unknown", capacity=0, current_vehicle_count=0, sensors=None, displays=None):
+    def __init__(self, location, capacity=0, plates = None,sensors=None, displays=None):
         self.location = location
-        self.sensors = []
         self.capacity = capacity
-        self.current_vehicle_count = current_vehicle_count
-        self.displays = []
+        self.plates = plates or []
+        self.sensors = sensors or []
+        self.displays = displays or []
 
     def  __str__(self):
         return f"Carpark at {self.location} with the capacity of {self.capacity} vehicles"
